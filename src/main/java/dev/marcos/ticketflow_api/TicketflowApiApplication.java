525d417e91b213592153ffb7c1cb7745eedee151
Marcos Allan <marcosallanm1to@gmail.com>
2025-12-20 12:12:04 -0300

    chore: init project with Spring Boot, JPA, Security and Lombok

---
+++ src/main/java/dev/marcos/ticketflow_api/TicketflowApiApplication.java
@@ -0,0 +1,13 @@
+package dev.marcos.ticketflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketflowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/marcos/ticketflow_api/TicketflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marcos.ticketflow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
