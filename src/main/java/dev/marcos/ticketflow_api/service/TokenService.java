32f8e1460f2d5ef11f82c658f916fc8a62a0f5c8
Marcos Allan <marcosallanm1to@gmail.com>
2025-12-20 17:12:53 -0300

    feat: add JWT utility class for generation and validation

---
+++ src/main/java/dev/marcos/ticketflow_api/service/TokenService.java
@@ -0,0 +1,51 @@
+package dev.marcos.ticketflow_api.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import dev.marcos.ticketflow_api.entity.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    private final Algorithm algorithm;
+
+    @Value("${spring.security.token.secret}")
+    private String secret;
+
+    @Value("${spring.security.token.issuer}")
+    private String issuer;
+
+    public TokenService(@Value("${spring.security.token.secret}") String secret,
+                        @Value("${spring.security.token.issuer}") String issuer) {
+        this.algorithm = Algorithm.HMAC256(secret);
+        this.secret = secret;
+        this.issuer = issuer;
+    }
+
+    public String generateToken(User user) {
+        return JWT.create()
+                .withIssuer(issuer)
+                .withSubject(user.getEmail())
+                .withClaim("id", user.getId().toString())
+                .withExpiresAt(genExpirationDate())
+                .sign(algorithm);
+    }
+
+    public String validateToken(String token) {
+        return JWT.require(algorithm)
+                .withIssuer(issuer)
+                .build()
+                .verify(token)
+                .getSubject();
+    }
+
+    private Instant genExpirationDate() {
+        return LocalDateTime.now().plusHours(24).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
